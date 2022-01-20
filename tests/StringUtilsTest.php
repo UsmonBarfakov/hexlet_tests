7f6a847e35bda361f208029156fe6992724afdc4
UsmonBarfakov <barfakov@gmail.com>
2022-01-20 13:44:05 +0500

    Add asserts by using webmozar/assert libriary

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -1,14 +1,10 @@
 <?php
+use Webmozart\Assert\Assert;
 use function StringUtils\capitalize;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-if (capitalize('hello') !== 'Hello') {
-    throw new \Exception('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-    throw new \Exception('Функция работает неверно!');
-}
+Assert::eq(capitalize('hello'),'Hello');
+Assert::eq(capitalize(''), '');
 
 echo 'Все тесты пройдены!';

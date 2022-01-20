a656142fcc281dbcae50e9fa1391bf4e0cde5655
UsmonBarfakov <barfakov@gmail.com>
2022-01-20 11:20:17 +0500

    add tests

---
+++ tests/StringUtilsTest.php
@@ -0,0 +1,14 @@
+<?php
+use function StringUtils\capitalize;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+if (capitalize('hello') !== 'Hello') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+if (capitalize('') !== '') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+echo 'Все тесты пройдены!';
